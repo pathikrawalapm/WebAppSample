157463e0cf6b32c088b05d786cc65b08787723f0
pathikms <pathikrawal@microsoft.com>
2022-09-02 16:44:23 +0100

    n

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Web.Configuration;
+using Npgsql;
+
 
 namespace WebApplicationSample
 {
@@ -11,6 +14,69 @@ namespace WebApplicationSample
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            var Host = WebConfigurationManager.AppSettings["Host"];
+            var User = WebConfigurationManager.AppSettings["User"];
+            var DBname = WebConfigurationManager.AppSettings["DBname"];
+            var Password = WebConfigurationManager.AppSettings["Password"];
+            var Port = WebConfigurationManager.AppSettings["Port"];
+
+            string connString =
+               String.Format(
+                   "Server={0};Username={1};Database={2};Port={3};Password={4};SSLMode=Prefer",
+                   Host,
+                   User,
+                   DBname,
+                   Port,
+                   Password);
+
+            using (var conn = new NpgsqlConnection(connString))
+
+            {
+                Console.Out.WriteLine("Opening the connection");
+                conn.Open();
+
+                using (var command = new NpgsqlCommand("DROP TABLE IF EXISTS inventory", conn))
+                {
+                    command.ExecuteNonQuery();
+                    // Console.Out.WriteLine("Finished dropping table (if existed)");
+
+                }
+
+                using (var command = new NpgsqlCommand("CREATE TABLE inventory(id serial PRIMARY KEY, name VARCHAR(50), quantity INTEGER)", conn))
+                {
+                    command.ExecuteNonQuery();
+                    //Console.Out.WriteLine("Finished creating table");
+                }
+
+                using (var command = new NpgsqlCommand("INSERT INTO inventory (name, quantity) VALUES (@n1, @q1), (@n2, @q2), (@n3, @q3)", conn))
+                {
+                    command.Parameters.AddWithValue("n1", "banana11");
+                    command.Parameters.AddWithValue("q1", 150);
+                    command.Parameters.AddWithValue("n2", "orange222");
+                    command.Parameters.AddWithValue("q2", 154);
+                    command.Parameters.AddWithValue("n3", "apple22");
+                    command.Parameters.AddWithValue("q3", 100);
+
+                    int nRows = command.ExecuteNonQuery();
+                    Console.Out.WriteLine(String.Format("Number of rows inserted={0}", nRows));
+                }
+            }
+
+            using (var conn = new NpgsqlConnection(connString))
+            {
+
+                // Console.Out.WriteLine("Opening connection");
+                conn.Open();
+
+
+                using (var command = new NpgsqlCommand("SELECT * FROM inventory", conn))
+                {
+
+                    var reader = command.ExecuteReader();
+                    GridView1.DataSource = reader;
+                    reader.Close();
+                }
+            }
 
         }
     }
